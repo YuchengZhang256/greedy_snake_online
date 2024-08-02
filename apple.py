1520bb147b56b11052d14c7b76b1d1f87d236fa0
YuchengZhang256 <1445074660x@gmail.com>
2024-08-02 16:55:04 +0800

    version 1.0

--- apple.py
+++ apple.py
@@ -11,19 +11,14 @@ class Apple(Sprite):
         self.settings = ai_game.settings 
         self.screen = ai_game.screen
         self.settings = ai_game.settings 
-        self.length = self.settings.apple_length
-        self.height = self.settings.apple_height
         self.color = self.settings.apple_color
+        self.apple_x = round(random.randrange(0, 1800 - 10) / 10.0) * 10.0
+        self.apple_y = round(random.randrange(0, 1200 - 10) / 10.0) * 10.0
         
-        """generate the position of the apple randomly, on both x-axis and  y-axis"""
-        rand_position_x = random.randint(200,1600)
-        rand_position_y = random.randint(100,1000)
-        self.rect = pygame.Rect(rand_position_x, rand_position_y, self.length, self.height)
-        
-        
+
     def draw_apple(self):
         """draw the apple on the screen"""
-        pygame.draw.rect(self.screen, self.color, self.rect)
+        pygame.draw.rect(self.screen, self.color, [self.apple_x, self.apple_y, 10, 10])
         
 
         
--- snake.py
+++ snake.py
@@ -5,13 +5,20 @@ class Snake:
     def __init__(self, sequence_number,ai_game):
         """the sequence number determines which player to manipulate this snake, for now this number is 0 or 1"""
         self.screen = ai_game.screen
-        self.snake_speed = 5
         self.block = 10
         self.length = 1
         self.list = []
         self.settings = Settings()
+        
+        """initialize the position of this snake due to its sequence number is 0 or 1"""
         self.x = self.settings.screen_length * sequence_number
         self.y = self.settings.screen_height * sequence_number
+        
+        """the initial state of this snake is still, you should use the keyboard to make it move"""
+        self.moving_right ==False
+        self.moving_left == False
+        self.moving_down == False
+        self.moving_up == False
         
         
     def draw_snake(self):
