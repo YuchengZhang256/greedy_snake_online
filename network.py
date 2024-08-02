55f326ac7c9a49a1b1b9246496e68698fbaf33f7
JiatongXie <jiatong-xie@g.ecc.u-tokyo.ac.jp>
2024-08-02 22:14:17 +0900

    fixed some bugs

--- network.py
+++ network.py
@@ -4,7 +4,7 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = '127.0.0.1'
+        self.server = socket.gethostbyname(socket.gethostname())
         self.port = 1145
         self.addr = (self.server, self.port)
         self.pos = self.connect()
