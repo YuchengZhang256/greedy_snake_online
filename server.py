b9b127d8fbec87db20d06482da7bf01996cdb84b
JiatongXie <jiatong-xie@g.ecc.u-tokyo.ac.jp>
2024-08-02 21:01:38 +0900

    two players can walk on the same screen

--- server.py
+++ server.py
@@ -2,39 +2,68 @@ import socket
 import threading
 import time
 
-HEADER = 64
 PORT = 1145
 SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECTION_MESSAGE = '!DISCONNECT'
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+
+try:
+    server.bind((SERVER, PORT))
+except socket.error as e:
+    str(e)
 
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+def read_pos(string):
+    string = string.split(',')
+    return int(string[0]), int(string[1])
 
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECTION_MESSAGE:
-                connected = False
-            print(f'[{addr}] {msg}')
+
+def make_pos(tup):
+    return str(tup[0]) + ',' + str(tup[1])
+
+
+pos = [(0, 0), (100, 100)]
+
+
+def handle_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
+    print(f"[NEW CONNECTION] {player} connected.")
+    reply = ''
+    while True:
+        try:
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
+
+            if not data:
+                print('[DISCONNECTION] Disconnected.')
+                break
+            else:
+                if player == 1:
+                    reply = pos[0]
+                if player == 0:
+                    reply = pos[1]
+
+                print('[RECEIVED] ', data)
+                print('[SENDING] ', reply)
+            conn.sendall(str.encode(make_pos(reply)))
+        except:
+            break
+
+    print('[DISCONNECTION] Lost connection.')
+    conn.close()
 
 
 def start():
-    server.listen()
+    server.listen(2)
     print(f"[LISTENING] The server started listening on {SERVER}")
+    current_player = 0
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        print('[CONNECTION] Connected to :', addr)
+        thread = threading.Thread(target=handle_client, args=(conn, current_player))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        current_player += 1
+        # print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 
 print('[STARTING] The server is starting...')
